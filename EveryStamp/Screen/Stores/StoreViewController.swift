9099c97b83aeb0c183521512cbf09a7102d01b80
zhaolin01 <zhaolin01@sunlands.com>
2018-10-21 20:32:34 +0800

    更新UI

--- EveryStamp/Screen/Stores/StoreViewController.swift
+++ EveryStamp/Screen/Stores/StoreViewController.swift
@@ -11,9 +11,6 @@ import UIKit
 class StoreViewController: UIViewController {
     
     @IBOutlet weak var searchBtn: UIButton!
-    @IBOutlet weak var nearbyBtn: UIButton!
-    @IBOutlet weak var recommendBtn: UIButton!
-    @IBOutlet weak var selectedview: UIView!
     @IBOutlet weak var tableView: UITableView!
     
     var disposebag: DisposeBag = DisposeBag()
@@ -33,38 +30,11 @@ class StoreViewController: UIViewController {
     }
     
     func bind() {
-        self.selectedview.snp.remakeConstraints({ maker in
-            maker.left.equalToSuperview().offset(self.distance)
-        })
-        
         _ = searchBtn.rx.tap.subscribe(onNext: { [weak self] _ in
             guard let `self` = self else { return }
             let storyboard = UIStoryboard(name: "SearchShopViewController", bundle: nil)
             let vc = storyboard.instantiateInitialViewController()
             self.navigationController?.pushViewController(vc!, animated: true)
-        }).disposed(by: disposebag)
-        
-        _ = nearbyBtn.rx.tap.subscribe(onNext: { [weak self] _ in
-            guard let `self` = self else { return }
-            self.recommendBtn.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
-            self.nearbyBtn.setTitleColor(#colorLiteral(red: 0.9764705882, green: 0.4862745098, blue: 0.1607843137, alpha: 1), for: .normal)
-            self.selectedview.snp.remakeConstraints({ maker in
-                maker.left.equalToSuperview().offset(self.distance)
-            })
-            self.isLatlng = true
-            self.requestData()
-        }).disposed(by: disposebag)
-        
-        _ = recommendBtn.rx.tap.subscribe(onNext: { [weak self] _ in
-            guard let `self` = self else { return }
-            self.recommendBtn.setTitleColor(#colorLiteral(red: 0.9764705882, green: 0.4862745098, blue: 0.1607843137, alpha: 1), for: .normal)
-            self.nearbyBtn.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
-            self.selectedview.snp.remakeConstraints({ maker in
-                maker.right.equalToSuperview().offset(-self.distance)
-            })
-            self.isLatlng = false
-            self.requestData()
-            
         }).disposed(by: disposebag)
     }
     
